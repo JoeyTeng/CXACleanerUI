b8770ba637c7259d7cd0baa88d3d916cc64662f9
Joey Teng <joey.teng.dev@gmail.com>
2017-07-15 02:37:52 +0800

    COMMIT

--- CXACleanerUI/test.cs
+++ CXACleanerUI/test.cs
@@ -10,7 +10,7 @@
 
 
 class Test {
-    public static void Main() {
+    public static void main() {
         int[,] node = new int[,] {{Constants.MappingConstants.DEFAULT}};
         RoutingApplication.Coordinate c = new RoutingApplication.Coordinate(0, 0);
 
@@ -21,7 +21,8 @@ class Test {
         System.Console.WriteLine("{0} {1}", Constants.MappingConstants.Blocked(node, c), Constants.MappingConstants.Unblocked(node, c));
 
         Constants.MappingConstants.Select(node, c);
-        System.Console.WriteLine("{0} {1}", Constants.MappingConstants.Selected(node, c), Constants.MappingConstants.Deselected(node, c));
+        System.Console.WriteLine("{0} {1} {2}", Constants.MappingConstants.Selected(node, c), Constants.MappingConstants.Deselected(node, c), node[0, 0]);
+        System.Console.WriteLine("{0} {1}", Constants.MappingConstants.SELECTED << Constants.MappingConstants.SELECTED_SHIFT, Constants.MappingConstants.SELECTED_MASK);
 
         Constants.MappingConstants.Deselect(node, c);
         System.Console.WriteLine("{0} {1}", Constants.MappingConstants.Selected(node, c), Constants.MappingConstants.Deselected(node, c));
