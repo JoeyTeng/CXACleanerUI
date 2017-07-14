46f5af5972636321f1520d90743eaf1e67a0ccbd
Joey Teng <joey.teng.dev@gmail.com>
2017-07-15 03:37:57 +0800

    Update test

--- CXACleanerUI/test.cs
+++ CXACleanerUI/test.cs
@@ -14,15 +14,15 @@ class Test {
         int[,] node = new int[,] {{Constants.MappingConstants.DEFAULT}};
         RoutingApplication.Coordinate c = new RoutingApplication.Coordinate(0, 0);
 
+        Constants.MappingConstants.Select(node, c);
+        System.Console.WriteLine("{0} {1} {2}", Constants.MappingConstants.Selected(node, c), Constants.MappingConstants.Deselected(node, c), node[0, 0]);
+        System.Console.WriteLine("{0} {1}", Constants.MappingConstants.SELECTED << Constants.MappingConstants.SELECTED_SHIFT, Constants.MappingConstants.SELECTED_MASK);
+
         Constants.MappingConstants.Block(node, c);
         System.Console.WriteLine("{0} {1}", Constants.MappingConstants.Blocked(node, c), Constants.MappingConstants.Unblocked(node, c));
 
         Constants.MappingConstants.Unblock(node, c);
         System.Console.WriteLine("{0} {1}", Constants.MappingConstants.Blocked(node, c), Constants.MappingConstants.Unblocked(node, c));
-
-        Constants.MappingConstants.Select(node, c);
-        System.Console.WriteLine("{0} {1} {2}", Constants.MappingConstants.Selected(node, c), Constants.MappingConstants.Deselected(node, c), node[0, 0]);
-        System.Console.WriteLine("{0} {1}", Constants.MappingConstants.SELECTED << Constants.MappingConstants.SELECTED_SHIFT, Constants.MappingConstants.SELECTED_MASK);
 
         Constants.MappingConstants.Deselect(node, c);
         System.Console.WriteLine("{0} {1}", Constants.MappingConstants.Selected(node, c), Constants.MappingConstants.Deselected(node, c));
